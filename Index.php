c2091eee4dae61ac858ad3291e4adfc02ba7aa39
FaizaDucale <113381562+FaizaDucale@users.noreply.github.com>
2022-10-31 21:53:07 -0400

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title> Jeopardy! </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<style> 
+body {
+  background-image: url('jeopback.jpg');
+}
+</style>
+
+<div class="fade-in-image">
+   <img src="jep.jpg" alt= "jeopardy" class = "center">
+</div>
+
+<a href="login.php">Click to Start Game</a>
+
+</body>
+</html>
